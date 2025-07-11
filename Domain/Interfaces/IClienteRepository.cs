1008b52050583795e3e04e90850192b913264910
lucianodvn <obrlucianodvn@hotmail.com>
2025-07-11 17:32:44 -0300

    first commit

---
+++ Domain/Interfaces/IClienteRepository.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces
+{
+    public interface IClienteRepository
+    {
+        Task<Cliente> SalvarAsync(Cliente cliente);
+        Task<Cliente> AtualizarAsync(Cliente cliente);
+        Task<Cliente> ObterPorIdAsync(int id);
+        Task<IEnumerable<Cliente>> ObterTodosCLientesAsync();
+        Task<bool> ExcluirAsync(int id);
+    }
+}
---
+++ Infrastructure/Context/AppContext.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Context
+{
+    public class AppContext : DbContext
+    {
+        public AppContext(DbContextOptions<AppContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
