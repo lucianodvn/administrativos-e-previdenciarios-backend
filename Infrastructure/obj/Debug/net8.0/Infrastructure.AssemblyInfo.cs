1bd64f3fba6a1d80364f562d2d5bde650e523e50
lucianodvn <obrlucianodvn@hotmail.com>
2025-07-22 12:08:51 -0300

    listar cliente e representantes

--- Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+324a779e49a5d7716c01d9335f7c9eb5f1671073")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0680395630d839ffa1e8f6118a44e91316f919c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
